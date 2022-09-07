49eff1e894156142dd848dae56f64de95db8e41a
mariazaltron <mariaemzaltron@gmail.com>
2022-09-07 23:41:42 +0200

    set up category by id router

--- Router/categoryRouter.js
+++ Router/categoryRouter.js
@@ -7,4 +7,15 @@ router.get("/", async (req, res, next) => {
   res.send(products);
 });
 
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const categoryById = await Category.findByPk(id);
+    !categoryById && res.status(404).send("Category not found");
+    res.send(categoryById);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
